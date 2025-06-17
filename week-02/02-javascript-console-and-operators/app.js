24e614a00308801c5134c1ad237c282d484028e3
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-06-17 13:23:25 +0100

    added more comments

--- week-02/02-javascript-console-and-operators/app.js
+++ week-02/02-javascript-console-and-operators/app.js
@@ -1,26 +1,30 @@
-let theWeather = "Sunny";
-const myFirstName = "Frankie";
+// Remember how we declare variables:
+let theWeather = "Sunny"; // 'let' if it can change
+const myFirstName = "Frankie"; // 'const' if it should remain the same
 
 // uh oh! The weather has changed
 theWeather = "Rainy"; // Because we 'let' our variable, we can change the value!
 
+// ----------
 // The console in our browsers inspector, shows us two things:
 // 1: What we have TOLD it to show us.
 // 2: Any errors which are occuring in our Javascript code.
 
-// this next line will create an error:
+// this next line, if we remove the '//'s, will create an error:
 // myFirstName = "Bob";
 
 console.log("Hello");
 
 // The way a console log works is:
-// we say, "Hey, "log" a message of some sort, to our console, on the browser
+// we say, "Hey computer, "log" a message of some sort, to our console, on the browser!"
 // In between the brackets is where we tell the computer what message we want
-console.log("Have a great day");
-console.log("Enjoy your meal");
-console.log(45);
-console.log("50");
+console.log("Have a great day"); //Output: "Have a great day"
+console.log("Enjoy your meal"); //Output: "Enjoy your meal"
+console.log(45); //Output: 45
+console.log("50"); //Output: ""50"
 
+// Check out how these next console.logs look in the browsers console.
+// Does the output change?
 console.log(theWeather);
 theWeather = "Misty";
 console.log(theWeather);
@@ -30,21 +34,23 @@ console.log(theWeather);
 console.log(theWeather);
 console.log(theWeather);
 
+// ----------
 // Javascript is super useful. We can perform operations
 const myFirstNumber = 10;
 const mySecondNumber = 20;
 const myCombinedNumber = myFirstNumber + mySecondNumber; // Is this value 30?
 // I'll write a console log to check the value!!
+console.log("The value of myCombinedNumber is: " + myCombinedNumber); //Output: "The value of myCombinedNumber is: 30"
 
-console.log("The value of myCombinedNumber is: " + myCombinedNumber);
-console.log(myCombinedNumber + myFirstNumber);
-console.log(30, 10);
-console.log(myCombinedNumber, myFirstNumber);
+// What if we use '+' to put things together?
+console.log(myCombinedNumber + myFirstNumber); //Output: 40
+console.log(30, 10); //Output: 30 10
+console.log(myCombinedNumber, myFirstNumber); //Output: 30 10
 console.log(
   "Please write this message",
   " and then, write this message",
   "and finally, write this message"
-);
+); //Output: "Please write this message  and then, write this message and finally, write this message"
 
 console.log("Frankie" + "Shrieves" + "is my name");
 const mySecondName = "Shrieves";
@@ -64,8 +70,8 @@ console.log(
   }`
 );
 
-console.log(typeof myCombinedNumber);
-console.log(typeof mySecondName);
+console.log(typeof myCombinedNumber); //Output: number
+console.log(typeof mySecondName); //Output: string
 
 // Some other operators for you to play with
 // + -> 'plus' adds or combines two things together
@@ -79,5 +85,4 @@ console.log(40 === 40); //play with this!
 // <   --> Check if something is lower than something else
 console.log(3 < 10); //true
 // >   --> Checks if something is greater than something else
-
 console.log(10 > 4); //true
