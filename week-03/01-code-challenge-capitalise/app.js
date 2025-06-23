623652421a12e996c9031a6712a6a5d082fbeec5
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-06-23 16:01:01 +0100

    capitalise code challenge kickoff

---
+++ week-03/01-code-challenge-capitalise/app.js
@@ -0,0 +1,24 @@
+// These logs are just to show that you can run the command: node app.js
+// in your terminal, and see your logs without a browser
+console.log("Hello world!")
+console.log(1+19)
+
+// Two forward slashes in Javascript gives you a comment
+// A VSCode shortcut is cmd+/
+// Or ctrl+/
+
+function capitaliseWord(word){
+    // in here is where I write my function code
+    // I need to figure out how to capitalise the first letter of the word
+
+
+    // Once I have capitalised the first letter, I must return the whole capitalised word
+    return word
+}
+
+// I must invoke my function, and I am console.log-ing the output:
+console.log(capitaliseWord("mousemat"))
+console.log(capitaliseWord("aeroplane"))
+
+// I strongly encourage that you type out the code which you are copying
+// I also encourage that you leave yourself comments as you think about the problem
