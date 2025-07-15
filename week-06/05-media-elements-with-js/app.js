b3f678469b534725e9976d86bc9cebf0a3ce9cf4
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-07-15 10:56:04 +0100

    media with js demo

---
+++ week-06/05-media-elements-with-js/app.js
@@ -0,0 +1,42 @@
+console.log("Its media time!");
+
+// I want to have some buttons on the page which control my audio
+
+// First, in my HTML I might want to display my audio
+// But I want individual buttons for play, pause, stop, FF and RW
+
+// My javascript needs to "see" things that are in my HTML
+const indianGuitarAudio = document.getElementById("indian-guitar-audio");
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+const ffButton = document.getElementById("ff-button");
+const rwButton = document.getElementById("rw-button");
+
+console.log(indianGuitarAudio);
+
+// My buttons need and "event listener" so that they can do a thing when an event happns
+playButton.addEventListener("click", function () {
+  // console.log("I have been clicked. Play the music maestro");
+  indianGuitarAudio.play();
+});
+
+pauseButton.addEventListener("click", function () {
+  // console.log("pause button clicked");
+  indianGuitarAudio.pause();
+});
+
+ffButton.addEventListener("click", function () {
+  console.log("ff button clicked");
+  indianGuitarAudio.currentTime = indianGuitarAudio.currentTime + 10;
+  // indianGuitarAudio.currentTime += 10 // exactly the same result
+});
+
+rwButton.addEventListener("click", function () {
+  console.log("rw button clicked");
+  indianGuitarAudio.currentTime = indianGuitarAudio.currentTime - 10;
+});
+
+// To 'stop' our audio:
+// indianGuitarAudio.stop() <-- this unfortunately doesnt work. COME ON JAVASCRIPT!!!
+// instead:
+// myAudio.pause(), myAudio.currentTime = 0
